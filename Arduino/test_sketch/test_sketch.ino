2a7351e4a744681450634edd115af1b2fe6c0fa5
Soran Ismail <sismai26@uwo.ca>
2019-10-12 20:17:22 -0400

    Changed Serial.print to Serial.write()

--- Arduino/test_sketch/test_sketch.ino
+++ Arduino/test_sketch/test_sketch.ino
@@ -1,8 +1,31 @@
 void setup() {
-  Serial.begin(9600); // opens serial port, sets data rate to 9600 bps
+  Serial.begin(9600);
 }
 
 void loop() {
-  Serial.print("Hello");
+  while(!Serial) {
+    ;
+  }
+  Serial.write('\n');
+  Serial.write('\r');
   
+  delay(1000);
+  
+  Serial.write('h');
+  
+  delay(500);
+  Serial.write('e');
+  
+  delay(500);
+  Serial.write('l');
+  
+  delay(500);
+  Serial.write('l');
+  
+  delay(500);
+  Serial.write('o');  
+  Serial.write('\n');
+  Serial.write('\r');
+
+  Serial.flush();
 }
